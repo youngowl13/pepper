680c5dd72ec7f9bb22058e6aceee484078760bd4
ashishsolankiofficial <ashishsolanki.acc@gmail.com>
2022-11-20 11:13:59 +0530

    product page done

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import list_view, get_images
+from .views import list_view, get_images, get_brands, get_categories
 
 urlpatterns = [
     path('', list_view),
     path('product-images/', get_images),
+    path('brands/', get_brands),
+    path('categories/', get_categories)
     # path('<str:ext_id>/', ProductDetails.as_view()),
 ]
