e98114ed92c586328b4527b8d853e3f68e32f2ac
ashishsolankiofficial <ashishsolanki.acc@gmail.com>
2023-02-20 12:44:43 +0530

    allowed host

--- pepper/settings.py
+++ pepper/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-15co#yq%u#n^i+n&+r-5+0mm=8jn^qvr#l-3&pau!zmdfs!#cv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.183.78.185']
 
 
 # Application definition
