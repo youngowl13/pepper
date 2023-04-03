75c5a9969ca77f72ac869232f15d59b6b24aa1f1
ashishsolankiofficial <ashishsolanki.acc@gmail.com>
2023-04-03 12:00:38 +0530

    postgres and pandas working

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM python:3.9.6-alpine
+# FROM python:3.9.6-alpine
+FROM nickgryg/alpine-pandas
 
 WORKDIR /usr/src/pepper
 
--- pepper/settings.py
+++ pepper/settings.py
@@ -81,17 +81,6 @@ WSGI_APPLICATION = "pepper.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'masterdb',
-#         'USER': 'ashish',
-#         'PASSWORD': 'pass123',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     "default": {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
